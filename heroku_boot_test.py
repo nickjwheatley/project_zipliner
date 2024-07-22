02e4bbdeb42f4854c5cfcaa6b79a9d7d6b35dafa
wheatleyn <55959180+wheatleyn@users.noreply.github.com>
2024-07-21 19:15:02 -0700

    add heroku boot test

---
+++ heroku_boot_test.py
@@ -0,0 +1,35 @@
+from dash import html, Dash, dash_table
+import pandas as pd
+import dash_bootstrap_components as dbc
+from data_extract import query_rds, get_rds_schema
+
+df_query = "SELECT metro,zip_code FROM prelim_merged_pivoted_snapshot LIMIT 10;"
+
+df = query_rds(df_query)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+app.layout = html.Div([
+        html.H1("Heroku Boot Test"),
+    html.Div([
+        dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records'),
+            style_table={'overflowX': 'auto'},
+            style_cell={'textAlign': 'left'},
+            style_header={
+                'backgroundColor': 'rgb(230, 230, 230)',
+                'fontWeight': 'bold'
+            },
+            style_data_conditional=[
+                {
+                    'if': {'row_index': 'odd'},
+                    'backgroundColor': 'rgb(248, 248, 248)'
+                }
+            ]
+        )
+    ])
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
