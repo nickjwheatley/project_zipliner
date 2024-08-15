121a3c0bbab58e669f096735ee3d1910902c8c69
Arun Suresh <arunvsuresh@gmail.com>
2024-08-14 18:00:06 -0700

    Update med_income.py

--- data/med_income.py
+++ data/med_income.py
@@ -24,7 +24,6 @@ Input files required:
 Output:
 - median_income.parquet
 
-Download link: `https://data.census.gov/table?q=S1903:%20Median%20Income%20in%20the%20Past%2012%20Months%20(in%202022%20Inflation-Adjusted%20Dollars)&g=010XX00US$8600000`
 """
 
 import pandas as pd
